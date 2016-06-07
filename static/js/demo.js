ba661b11f8d8dd8ab201037483f0268c7acf639c
Fan <fancarter007@163.com>
2016-06-07 16:54:00 +0800

    redo editor

--- static/js/demo.js
+++ static/js/demo.js
@@ -22,8 +22,9 @@ $(function () {
                             newTxt(box, place);
                             break;
                         case 'richTxt':
-                            var EditorId = thisName + '-edit';
-                            newEditor(box, EditorId);
+                            //var EditorId = thisName + '-edit';
+                            //newEditor(box, EditorId);
+                            newEditor(box, place);
                             break;
                         case 'btn':
                             newBtn(box, place);
@@ -37,33 +38,64 @@ $(function () {
 
     //富文本
 
-    function newEditor(box, placeholderID) {
-        var settings = {
-            toolbars: [
-                [   'undo','redo','|','bold', 'italic',  'underline',  'forecolor', '|', 'cleardoc',    '|','link',  '|', 'justifyleft', 'justifyright',  'justifycenter', '|', 'unlink'],
-                ['source', 'fontfamily', 'fontsize' ,'addbr']
-            ]
-            ,wordCount:false
-        };
-        var ue = UE.getEditor(placeholderID, settings);
-        var text = box.html().trim();
-        var pID = placeholderID.substring(0,(placeholderID.length - 5));
-        $('#'+pID).append('<div class="J-ph">加载中...</div>');
-        text = text.replace(/\s+|\n/g, " ").replace(/>\s</g, "><");
-        ue.ready(function(){
-            $('#'+pID).find('.J-ph').hide();
-            ue.setContent(text);
-            ue.addListener('contentChange', function (ue) {
-                var inner = this.getContent();
-                box.html(inner);
-                var $links = box.find('a');
-                if ($links) {
-                    $links.css({color: '#246bb3', textDecoration: 'none'})
+    //function newEditor(box, placeholderID) {
+    //    var settings = {
+    //        toolbars: [
+    //            [   'undo','redo','|','bold', 'italic',  'underline',  'forecolor', '|', 'cleardoc',    '|','link',  '|', 'justifyleft', 'justifyright',  'justifycenter', '|', 'unlink'],
+    //            ['source', 'fontfamily', 'fontsize' ,'addbr']
+    //        ]
+    //        ,wordCount:false
+    //    };
+    //    var ue = UE.getEditor(placeholderID, settings);
+    //    var text = box.html().trim();
+    //    var pID = placeholderID.substring(0,(placeholderID.length - 5));
+    //    $('#'+pID).append('<div class="J-ph">加载中...</div>');
+    //    text = text.replace(/\s+|\n/g, " ").replace(/>\s</g, "><");
+    //    ue.ready(function(){
+    //        $('#'+pID).find('.J-ph').hide();
+    //        ue.setContent(text);
+    //        ue.addListener('contentChange', function (ue) {
+    //            var inner = this.getContent();
+    //            box.html(inner);
+    //            var $links = box.find('a');
+    //            if ($links) {
+    //                $links.css({color: '#246bb3', textDecoration: 'none'})
+    //            }
+    //        })
+    //    })
+    //}
+
+
+    function newEditor (box,place){
+        var richTpl = $("#richTxtPlaceTpl").html();
+        var tplPlace = template(richTpl);
+        place.append(tplPlace);
+
+        var txt = box.html().trim();
+        var styles = box.attr('data-styles');
+
+        place.find('.rich-box').html(txt);
+
+
+        place.on('focus','.rich-box',function(e){
+            var _this = $(this);
+            e.stopPropagation();
+            _this.bind('keyup',function(e){
+                e.stopPropagation();
+                e.preventDefault();
+                if (e.which === 13){
+
+                    console.log('enter')
                 }
             })
+        }).on('blur','.rich-box',function(){
+            var _this = $(this);
+            _this.unbind('keyup');
+            var inner = _this.html().trim();
+            box.html('').html(inner);
         })
-    }
 
+    }
 
     //图片
 
@@ -635,7 +667,7 @@ $(function () {
         var obtarget = document.querySelector('#tableInner');
         var observer = new MutationObserver(function(mutations) {
             mutations.forEach(function(mutation) {
-                console.log(mutation);
+                //console.log(mutation);
                 isSaved = false;
             });
         });
